af4663cdfa2af0ae2d1f4e721ade04dc74bc239d
Biplob Sutradhar <biplobsd11@gmail.com>
2021-02-03 09:59:43 +0600

    Update ocr.py

--- res/ocr.py
+++ res/ocr.py
@@ -536,7 +536,7 @@ class PortForward:
     self.connections=c
     if config:config[1] = closePort(config[1])
     self.config = config
-    if SERVICE="ngrok":self.ngrok=ngrok(TOKEN,USE_FREE_TOKEN,connections,region,self.config)
+    if SERVICE=="ngrok":self.ngrok=ngrok(TOKEN,USE_FREE_TOKEN,connections,region,self.config)
     self.SERVICE = SERVICE
     
 
